722b3585b732299669a5d0d5fb11f4bcd5ae38c7
Md Shahadat Hossen Shuvo <85306829+shuvo352865@users.noreply.github.com>
2023-09-18 23:16:42 +0600

    Automatic_water_pump

--- Water_Level_1.ino
+++ Water_Level_1.ino
@@ -1,13 +1,46 @@
-void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+#define BLYNK_TEMPLATE_ID "TMPLsIRSWk4m"
+#define BLYNK_DEVICE_NAME "Water tank monitor and control"
+#define BLYNK_AUTH_TOKEN "ux3xaeQKfr2-hPLOp3md4YIJaQOUSyxu"
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define trig D5   // Trig pin
+#define echo D6 
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "shahadat";
+char pass[] = "12341234";
+int depth =8;
+BlynkTimer timer;
+void setup()
+{
+  pinMode(trig, OUTPUT);
+  pinMode(D0, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(10L, waterlevel);
 }
-
-    // the loop function runs over and over again forever
-    void loop() {
-    digitalWrite(LED_BUILTIN, LOW);     // Turn the LED on (Note that LOW is the voltage level
-                                        // but actually the LED is on; this is because 
-                                        // it is acive low on the ESP-01)
-    delay(1000);                        // Wait for a second
-    digitalWrite(LED_BUILTIN, HIGH);    // Turn the LED off by making the voltage HIGH
-    delay(2000);                        // Wait for two seconds (to demonstrate the active low LED)
+void waterlevel()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long t = pulseIn(echo, HIGH);
+  long cm = t / 29 / 2;
+  Serial.println(cm);
+  long level= depth-cm;
+  if (level<0)
+  level=0;
+  level = map(level,0,depth-3,0,100);
+  Blynk.virtualWrite(V0, level);
 }
+BLYNK_WRITE(V1) {
+  digitalWrite(D0, param.asInt());
+}
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  }
